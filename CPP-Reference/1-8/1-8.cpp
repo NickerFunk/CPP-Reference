553d71814084f4fc6129d0f8e2153293039187bc
NickerFunk <vincent.nguyennf@gmail.com>
2020-04-11 23:30:42 -0600

    Set 1, Variable, Operator & Expression - COMPLETE

---
+++ CPP-Reference/1-8/1-8.cpp
@@ -0,0 +1,34 @@
+
+// 1-8.cpp
+// Write a program to calculate area of circle.
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using std::cout;
+using std::cin;
+using std::setw;
+using std::endl;
+
+float areaOfCircle(float* fRadius);
+
+int main()
+{
+	float radius, output;
+
+	cout << "\n  Enter the length of the circle's radius (in units): ";
+	cin >> radius;
+	radius = pow(radius, 2);
+
+	output = areaOfCircle(&radius);
+
+	cout << "\n\n  After calculating using pi (3.14) in this instance. The answer would be " << output << " units\n\n";
+}
+
+float areaOfCircle(float* fRadius)
+{
+	float pi = 3.14;
+
+	return (*fRadius * pi);
+}
